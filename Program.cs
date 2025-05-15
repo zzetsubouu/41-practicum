196818ea35a660cc128083dc5238c2da35314878
Kab-35-16 <Kab-35-16@KAB-31-7->
2025-05-15 11:17:51 +0300

    .

--- Program.cs
+++ Program.cs
@@ -8,50 +8,8 @@ namespace _41_practicum
 {
     class Program
     {
-        static double Min(double x, double y)
-        {
-            if (x > y)
-            {
-                return y;
-            }
-            else if (x < y)
-            {
-                return x;
-            }
-            else
-            {
-                return 0;
-            }
-        }
-        static double Max(double x, double y)
-        {
-            if (x > y)
-            {
-                return x;
-            }
-            else if (x < y)
-            {
-                return y;
-            }
-            else
-            {
-                return 0;
-            }
-        }
-        delegate double HigherOrGreater(double x, double y);
         static void Main(string[] args)
         {
-            HigherOrGreater min = Min;
-            HigherOrGreater max = Max;
-
-            Console.WriteLine("введите первое число");
-            double num1 = double.Parse(Console.ReadLine());
-            Console.WriteLine("введите второе число");
-            double num2 = double.Parse(Console.ReadLine());
-
-            Console.WriteLine(min(num1, num2));
-            Console.WriteLine(max(num1, num2));
         }
     }
-
 }
